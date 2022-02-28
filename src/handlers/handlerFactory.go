0686925b02d51925c93d0e37082a2c132a4ea70f
fajry hamzah <fhaji.jakmall@gmail.com>
2022-02-28 18:02:42 +0700

    feat: implement handler and handler factory

--- src/handlers/handlerFactory.go
+++ src/handlers/handlerFactory.go
@@ -3,8 +3,15 @@ package handlers
 import (
 	"github.com/fajryhamzah/worclue/src/enums"
 	"github.com/fajryhamzah/worclue/src/exceptions"
+	"github.com/fajryhamzah/worclue/src/handlers/games/katla"
 )
 
 func createHandler(gameInput enums.GameInput) GameHandlerInterface {
+	switch gameInput.Code {
+	case katla.CODE:
+		return katla.Katla{}
+	}
+
 	exceptions.Throw(422, "Game handler not found")
+	panic("to make linter silent")
 }
--- src/handlers/mainHandler.go
+++ src/handlers/mainHandler.go
@@ -4,4 +4,8 @@ import "net/http"
 
 func MainHandler(w http.ResponseWriter, r *http.Request) {
 	gameInput := createGameInput(r)
+
+	handler := createHandler(gameInput)
+
+	handler.GetAnswer(gameInput)
 }
