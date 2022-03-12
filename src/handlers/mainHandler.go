ff512846b219dbbd7ba9e2ef4098cadce89cae62
fajry hamzah <fhaji.jakmall@gmail.com>
2022-03-12 12:07:17 +0700

    feat: serve json response from game handler

--- src/handlers/mainHandler.go
+++ src/handlers/mainHandler.go
@@ -1,11 +1,19 @@
 package handlers
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 func MainHandler(w http.ResponseWriter, r *http.Request) {
 	gameInput := createGameInput(r)
 
 	handler := createHandler(gameInput)
+	answer := handler.GetAnswer(gameInput)
 
-	handler.GetAnswer(gameInput)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	json.NewEncoder(w).Encode(answer)
+
 }
